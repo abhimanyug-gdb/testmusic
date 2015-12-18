18988c40032ce81868bd7749a27ce387bf8d6ab4
Abhimanyu Gite <abhimanyu_gite@hotmail.com>
2015-12-18 16:20:12 +0530

    Add all options to navigation drawer with icons

--- TestMusic/app/src/main/java/com/gdb/testmusic/MainActivity.java
+++ TestMusic/app/src/main/java/com/gdb/testmusic/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -41,13 +42,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -68,17 +62,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.action_music) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.action_shows) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.action_profile) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.action_created_playlists) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.action_hearted_playlists) {
+
+        } else if (id == R.id.action_settings) {
 
         }
 
